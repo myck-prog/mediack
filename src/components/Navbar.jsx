522cc8611a2164dd8d33c7d9b39d45ecb347b141
myck-prog <myckland.learnprog@gmail.com>
2022-08-31 10:41:51 -0700

    Sidebar at Feed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import { Stack } from '@mui/system';
 import { Link } from 'react-router-dom';
 
 import {logo} from '../utils/constants'
+import SearchBar from './SearchBar';
 
 const Navbar = () => (
   <Stack 
@@ -14,6 +15,7 @@ const Navbar = () => (
     <Link to="/" style={{display: 'flex',alignItems:'center'}}>
       <img src={logo} alt="logo" height={45} />
     </Link>
+    <SearchBar />
   </Stack>
 )
 export default Navbar
